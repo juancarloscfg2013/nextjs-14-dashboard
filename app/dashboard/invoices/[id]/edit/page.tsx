a794d34be3eaa8e343cfd5cae728f0f88f25189f
juank <juancarloscfg2013@gmail.com>
2023-10-29 10:23:49 -0700

    chapter 13 done(handling errors)

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,11 +1,12 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
     const invoice = await fetchInvoiceById(id);
-    if(invoice === null) return null;
+    if(!invoice) notFound();
     const customers = await fetchCustomers();
   return (
     <main>
